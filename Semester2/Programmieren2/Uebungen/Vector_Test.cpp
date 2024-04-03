856a337c95535bb480f07485a928239b8b63a896
pawx95 <57683375+kleipert@users.noreply.github.com>
2024-04-03 08:00:10 +0200

    [ADD] testcase for vector class

--- Semester2/Programmieren2/Uebungen/Vector_Test.cpp
+++ Semester2/Programmieren2/Uebungen/Vector_Test.cpp
@@ -10,3 +10,16 @@ int main()
     }
     std::cout << v1;
 }
+
+void TestCompare(){
+    Vector v1 = Vector(5);
+    Vector v2 = Vector(5);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        v1[i] = i;
+        v2[i9 = i;
+    }
+
+    std::cout << "Compare: Should return true -> " << v1 == v2;
+}
