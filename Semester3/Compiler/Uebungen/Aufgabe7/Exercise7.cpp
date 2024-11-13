8b7095c70d5045145bc7af371e971f7ba9b731cc
Kevin <kleipert@web.de>
2024-11-13 04:20:10 +0100

    Comipler Aufgabe 7

---
+++ Semester3/Compiler/Uebungen/Aufgabe7/Exercise7.cpp
@@ -0,0 +1,79 @@
+#include "iostream"
+
+int scanner_a()
+{
+    int state=0;
+    char input=' ';
+    while (input !='\n')
+    {
+        input = getchar(); 
+        switch (state)
+        {
+            // as many "a" as we want or one "b"
+            case 0:
+                if (input =='b')
+                    state = 1;
+                else if (input != 'a') 
+                    state = -1; 
+                break;
+            case 1:
+                if (input != '\n')
+                    state = -1;
+                break;
+            default:
+                state=-1;
+                break;
+        }
+    }
+    if (state == 1)
+        printf("Input valid!\n");
+    else
+        printf("Input not valid!\n");
+    return 0;
+}
+
+int scanner_b()
+{
+    int state=0;
+    char input=' ';
+    while (input !='\n')
+    {
+        input = getchar();
+        switch (state)
+        {
+            // as many "a" as we want or one "b"
+            case 0:
+                if (input =='0')
+                    state = 1;
+                else
+                    state = -1;
+                break;
+            case 1:
+                if (input =='1')
+                    state = 2;
+                else
+                    state = -1;
+                break;
+            case 2:
+                if ((input !='1') && (input !='2') && (input != '\n'))
+                    state = -1;
+                break;
+            default:
+                state=-1;
+                break;
+        }
+    }
+    if (state == 2)
+        printf("Input valid!\n");
+    else
+        printf("Input not valid!\n");
+    return 0;
+}
+
+int main()
+{
+    std::cout << "Enter your Input and press Enter:\n" << scanner_a();
+    std::cout << "Enter your Input and press Enter:\n" << scanner_b();
+    
+    return 0;
+}
